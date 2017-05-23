e1ac5bc684515858fe0f5983b952491199b3e863
Daniel Brandt <dbbrandt@gmail.com>
2017-05-22 23:13:47 -0400

    Add fae::viewhelper to support sorting and other features from fae.
    Some helpers like fae_delete_button require the fae::application_helper which is required for the fae_delete_button and addes security scope. It's easier to just navigate to the admin pages for these features.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :set_option
+
+  helper Fae::ViewHelper
+
+  def set_option
+    @option = Fae::Option.instance
+  end
+
 end
