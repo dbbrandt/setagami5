372216b46e7ad73ea75bd36a353fc2053d9acb11
Daniel Brandt <dbbrandt@gmail.com>
2017-05-23 12:46:51 -0400

    No change

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170522211847) do
+ActiveRecord::Schema.define(version: 20170523150718) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -207,6 +207,7 @@ ActiveRecord::Schema.define(version: 20170522211847) do
     t.text "quiz"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "incorrect"
   end
 
   add_foreign_key "people", "imdbs"
